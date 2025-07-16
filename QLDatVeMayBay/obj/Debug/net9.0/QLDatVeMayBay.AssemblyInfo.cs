40a1bff33c5bbb04e7564d85bae3a8f07669c8b1
danghieu7bthcsnh <121305135+danghieu7bthcsnh@users.noreply.github.com>
2025-07-16 13:31:04 +0700

    fina

--- QLDatVeMayBay/obj/Debug/net9.0/QLDatVeMayBay.AssemblyInfo.cs
+++ QLDatVeMayBay/obj/Debug/net9.0/QLDatVeMayBay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLDatVeMayBay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f75577fdd49d4134ed59463f678466308ca1b62e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b883b652921e035a3937ccf89d46f564058dfdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLDatVeMayBay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLDatVeMayBay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
